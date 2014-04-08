2c0785b3833ce3be59d9c19a34242964d24c10bb
anatoliliotych <anatoli@rightscale.com>
2014-04-08 18:53:14 +0300

    Implement issue #13 with notification for expiring vacation

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -14,6 +14,19 @@ module Sinatra
         month = t(month)
         "#{month} #{year}"
       end
+
+      def highlight_vac(start_date,vac_cur_year)
+        current_date = Time.now.strftime("%Y-%m-01").split('-')
+        start_date = start_date.strftime("%m-%d").split('-')
+        start_date.unshift Time.now.year
+        end_of_work_year = start_date
+        if Time.new(*current_date) > Time.new(*start_date)
+           end_of_work_year.shift
+           end_of_work_year.unshift(Time.now.year + 1)
+        end
+        time = Time.new(*end_of_work_year) - Time.new(*current_date)
+        vac_cur_year.to_i > 64 && (time / 3600 * 24 * 30) <= 3
+      end
     end
   end
 end
